a2c2064ad2b1f30ce1d9d99289604c75b6d23c57
Aminata Kabba <aminata-kabba@hotmail.com>
2021-06-17 00:31:16 +0200

    updated version of the commandline application, optimised and more consise

--- comandline.py
+++ comandline.py
@@ -1,105 +1,50 @@
 # Imported libraries that will be required 
 import requests # helps fetching apis
-import pprint # makes json files much more readable
+import pprint
 import time
 
 # ========== INTRODUCTION TO THE APP ================
-print('Hello, bookholic :), welcome to Ami\'s library')
+print('Hello, bookholic :), welcome to Ami\'s library \n')
 time.sleep(2) # Allows user to have sometime to read
-print('This is the best place to find your favourite books, new and interesting reads')
+print('This is the best place to find your favourite books, new and interesting reads \n')
 time.sleep(2) # Allows user to have sometime to read
 url = "https://www.googleapis.com/books/v1/volumes?q=" # API URL
 interest = input('What interests you?  You can write the author, book title or genra that comes to your mind \n') # User will add what type of books they're looking for
 urlInterest = str(url + interest) # Attaches the standard url to the input from the user
 
 # =============== BOOKS SECTION ====================== 
-print("This is the list of the 5 closes books to what you want \n")
+print("This is the list of the 5 closest books to what you want \n")
 
-authSent = "Author: " 
-titleSent = "Title: "
-pubSent = "Publisher: "
 
-#Book Number 1
-author = requests.get(urlInterest).json()['items'][0]['volumeInfo']['authors'][0] # Finds the author
-print(authSent + author)
-title = requests.get(urlInterest).json()['items'][0]['volumeInfo']['title'] # Finds the title
-print(titleSent + title)
-publishing = requests.get(urlInterest).json()['items'][0]['volumeInfo']['publisher'] # Finds the publishing company
-print(pubSent + publishing)
-print("\n")
+requesting = requests.get(urlInterest).json()
+items = requesting['items']
 
-#Book Number 2
-author2 = requests.get(urlInterest).json()['items'][2]['volumeInfo']['authors'][0]
-print(authSent + author2)
-title2 = requests.get(urlInterest).json()['items'][2]['volumeInfo']['title']
-print(titleSent + title2)
-publishing2 = requests.get(urlInterest).json()['items'][2]['volumeInfo']['publisher']
-print(pubSent + publishing2)
-print("\n")
-
-#Book Number 3
-author3 = requests.get(urlInterest).json()['items'][3]['volumeInfo']['authors'][0]
-print(authSent + author3)
-title3 = requests.get(urlInterest).json()['items'][3]['volumeInfo']['title']
-print(titleSent + title3)
-publishing3 = requests.get(urlInterest).json()['items'][3]['volumeInfo']['publisher']
-print(pubSent + publishing3)
-print("\n")
-
-#Book Number 4
-author4 = requests.get(urlInterest).json()['items'][4]['volumeInfo']['authors'][0]
-print(authSent + author4)
-title4 = requests.get(urlInterest).json()['items'][4]['volumeInfo']['title']
-print(titleSent + title4)
-publishing4 = requests.get(urlInterest).json()['items'][4]['volumeInfo']['publisher']
-print(pubSent + publishing4)
-print("\n")
-
-#Book Number 5
-author5 = requests.get(urlInterest).json()['items'][5]['volumeInfo']['authors'][0]
-print(authSent + author5)
-title5 = requests.get(urlInterest).json()['items'][5]['volumeInfo']['title']
-print(titleSent + title5)
-publishing5 = requests.get(urlInterest).json()['items'][5]['volumeInfo']['publisher']
-print(pubSent + publishing5)
-print("\n")
-
+for item in items:
+    author = item['volumeInfo']['authors'] # Find the authors
+    title = item['volumeInfo']['title'] # Find the title of the book
+    publisher = item['volumeInfo']['publisher'] # Find the publishing company
+    print("AUTHORS: " )
+    print(author)
+    print("TITLE: \n" + title)
+    print("PUBLISHER: \n" + publisher)    
+    print("\n")
 
 # ============ ADDING ITEMS TO THE READING LIST ==========
 readingList = input("Would you like to add any books into your reading list? Y/N ")
 if readingList == 'Y' or readingList == 'y':
-    list = []
-    # Options to add the book to your reading list 
-    print('Press \'a\' if you want to add the book to your reading list and any other letter if not')
-    time.sleep(2)
-    one = input(title + ' ')
-    print(one)
+    list = [] # Items that the costumer wants on their reading list will be added to this empty list
+    print('Press \'a\' if you want to add it to your reading list or any other if not \n') # Instructions on how to add items to the reading list
+    for item in items:
+        readingListTitle = item['volumeInfo']['title'] # Shows the title of the book
+        time.sleep(1)
+        question = input(readingListTitle + " ")
+        print(question)
 
-    two = input(title2+ ' ')
-    print(two)
-
-    three = input(title3 + ' ')
-    print(three)
-
-    four = input(title4 + ' ')
-    print(four)
-
-    five = input(title5 + ' ')
-    print(five)
-    
-    # If the user replied a it will be added to the list
-    if one == 'a':
-        list.append(title)
-    if two == 'a':
-        list.append(title2)
-    if three == 'a':
-        list.append(title3)
-    if four == 'a':
-        list.append(title4)
-    if five == 'a':
-        list.append(title5)
-    print("This is your reading list, enjoy and hope to see you soon") # End of the program
-    time.sleep(1)
-    print(list)
-elif readingList == "N" or readingList == 'n':
-    print("Hava a nice day and thats for coming to Ami's library") # End of the program
+        if question == 'a':
+            list.append(readingListTitle) # Only the title of the book will be added to the reading list
+    print("THIS IS YOOUR READING LIST \n")
+    lists = pprint.pprint(list)
+    print(lists)
+    print("\n Thanks for coming to Ami\'s library, we hope to see you soon")
+else:
+    print("Thanks for coming to Ami\'s library, have an amazing day and see you soon :)")
